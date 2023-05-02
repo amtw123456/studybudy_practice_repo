34eda5dfb79b6914043c9f8bc5dd46cb2222a0c5
amtw123456 <radolor@up.edu.ph>
2023-05-02 17:02:15 +0800

    Fourth commit Finished crud part

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import Room
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import render, redirect
+from .serializers import RoomSerializer
+from .models import Room
+from .forms import RoomForm
 # rooms = [
 #     {'id':1, 'name':'Lets learn python!'},
 #     {'id':2, 'name':'Design with me'},
@@ -8,6 +12,12 @@ from .models import Room
 # ]
 
 # Create your views here.
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
 def home(request):
     room = Room.objects.all()
     context = {'rooms':room}
@@ -17,3 +27,34 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'api/room.html', context)
+
+def createRoom(request): # POST METHOD THAT SAVES A NEW ROOM DATA TO THE api_room colleciton
+    form = RoomForm()
+    if request.method == 'POST':
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'api/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'api/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'api/delete.html', {'obj':room})
