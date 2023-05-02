d13a210623c0ffb883e6de43922fd73fd4f33978
amtw123456 <radolor@up.edu.ph>
2023-05-02 12:08:34 +0800

    Second study buddy commit finished template video part

--- api/views.py
+++ api/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend Developers'},
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'api/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
+    return render(request, 'api/room.html', context)
